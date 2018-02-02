8dfde5252a6e337e0ab5fa94e4bf589edd1cc878
ftmkk <ftm.karimkhani@gmail.com>
2018-02-03 00:57:34 +0330

    Repository Package added.

--- src/main/java/Model/DatabaseOperations/Elasticsearch.java
+++ src/main/java/Model/DatabaseOperations/Elasticsearch.java
@@ -1,5 +1,6 @@
 package Model.DatabaseOperations;
 
+import Model.Entities.Feedback.FeedbackManagement;
 import Model.Entities.ProductInfo.*;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.search.SearchRequest;
@@ -11,6 +12,7 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.query.AbstractQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.search.MatchQuery;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -33,37 +35,38 @@ public class Elasticsearch {
     private RestClient client;
     private static Gson gson;
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
 
         List<Attribute> attrL1 = new ArrayList<>();
-        AttributeGroup attrG = new AttributeGroup("CATEGORY_1","kategory");
-        attrL1.add(new Attribute("wight","2.3 kg",attrG));
+        AttributeGroup attrG = new AttributeGroup("CATEGORY_1", "category");
+        attrL1.add(new Attribute("wight", "2.3 kg", attrG));
         List<Color> colors1 = new ArrayList<>();
-        colors1.add(new Color("White","sefid", 0,0,0));
+        colors1.add(new Color("White", "sefid", 0, 0, 0));
 
-/*        Product p1 = new Product(
-                "p1",
-                "P1",
-                50000000L,
+        Product p1 = new Product(
+                "p3",
+                "P3",
+                13000000L,
                 ProductStatus.AVAILABLE,
-                233,
-                "THis is product p1!",
-                2234,
-                923,
+                3,
+                "THis is product p3!",
+                91992,
+                93,
                 null,
                 "THis is review of product p1!",
-                new Brand("Samsung","SAMSUNG"),
+                new Brand("apple","apple"),
                 attrL1,
                 colors1,
-                new Guaranty("12 months MICROTELL"),
-                3000L);*/
+                new Guaranty("12 months avajang"),
+                3000L,
+                new FeedbackManagement());
 
         Client client = getClient();
         //client.prepareDelete("onlineshopping","product","AWDHvwg_W2MtLdBt5RiP").execute().actionGet();
         //deleteDocument("AWDav5uTv0ci70SiEVmd");
-        //add(p1);
-        List<Product> products = matchAllQuery();
-        System.out.println(products.size());
+        add(p1);
+        //MustQueryName("this");
+
     }
 
 
@@ -91,38 +94,42 @@ public class Elasticsearch {
             builder = jsonBuilder()
                     .startObject()
                     .field("name", product.getName())
-                    .field("persian_name",product.getPersianName())
+                    .field("persian_name", product.getPersianName())
                     .field("price", product.getPrice())
-                    .field("status" , product.getStatus())
-                    .field("remainingCount",product.getRemainingCount())
-                    .field("briefDescription",product.getBriefDescription())
-                    .field("visitCount",product.getVisitCount())
-                    .field("sellCount",product.getSellCount())
-                    .field("releaseDate",product.getReleaseDate())
-                    .field("review",product.getReview())
-                    .startObject("Brand").field("name",product.getBrand().getName()).field("persian_name",product.getBrand().getPersianName()).endObject()
-                    .startObject("attributes").field("value",product.getAttributes().get(0).getValue()).field("Title",product.getAttributes().get(0).getTitle()).endObject()
-                    .startObject("colors").field("name",product.getColors().get(0).getName()).field("perian_name",product.getColors().get(0).getPersianName()).field("Color_R",product.getColors().get(0).getR()).field("Color_G",product.getColors().get(0).getG()).field("Color_B",product.getColors().get(0).getB()).endObject()
-                    .startObject("guaranty").field("name",product.getGuaranty().getId()).field("name",product.getGuaranty().getName()).endObject()
-                    .field("discount",product.getDiscount())
+                    .field("status", product.getStatus())
+                    .field("remainingCount", product.getRemainingCount())
+                    .field("briefDescription", product.getBriefDescription())
+                    .field("visitCount", product.getVisitCount())
+                    .field("sellCount", product.getSellCount())
+                    .field("releaseDate", product.getReleaseDate())
+                    .field("review", product.getReview())
+                    .startObject("Brand").field("name", product.getBrand().getName()).field("persian_name", product.getBrand().getPersianName()).endObject()
+//                    .startObject("attributes").field("value", product.getAttributes().get(0).getValue()).field("Title", product.getAttributes().get(0).getTitle()).endObject()
+                    //                   .startObject("colors").field("name", product.getColors().get(0).getName()).field("perian_name", product.getColors().get(0).getPersianName()).field("Color_R", product.getColors().get(0).getR()).field("Color_G", product.getColors().get(0).getG()).field("Color_B", product.getColors().get(0).getB()).endObject()
+                    //                   .startObject("guaranty").field("name", product.getGuaranty().getId()).field("name", product.getGuaranty().getName()).endObject()
+                    .field("discount", product.getDiscount())
                     .endObject();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.print(e);
         }
-        client.prepareIndex()
-                .setIndex("onlineshopping")
-                .setType("product")
-                .setSource(builder).execute().actionGet();
+        try {
+            client.prepareIndex()
+                    .setIndex("onlineshopping")
+                    .setType("product")
+                    .setSource(builder).execute().actionGet();
+        }catch (Exception k){
+            System.out.print(k);
+        }
     }
 
 
-    public static List<Product> matchAllQuery(){
+    public static List<Product> matchAllQuery() {
 
         List<Product> result = new ArrayList<>();
 
         try {
             result = getDocuments(QueryBuilders.matchAllQuery());
-        } catch (Exception ex){
+        } catch (Exception ex) {
             logger.info("The exception was thrown in matchAllQuery method." + ex);
         }
 
@@ -147,11 +154,12 @@ public class Elasticsearch {
         return result;
     }
 
-    private static SearchRequest getSearchRequest(){
+    private static SearchRequest getSearchRequest() {
         SearchRequest searchRequest = new SearchRequest("onlineshopping");
         searchRequest.source(sourceBuilder);
         return searchRequest;
     }
+
 
     public static void deleteDocument(String id) throws UnknownHostException {
         Client client = getClient();
@@ -163,15 +171,43 @@ public class Elasticsearch {
         }
     }
 
-    public List<Product> wildcardQuery(String query){
+    public List<Product> wildcardQuery(String searchTerm){
 
         List<Product> result = new ArrayList<>();
 
         try {
-            result = getDocuments(QueryBuilders.wildcardQuery("_all", "*" + query.toLowerCase() + "*"));
+            result = getDocuments(QueryBuilders.wildcardQuery("_all", "*" + searchTerm.toLowerCase() + "*"));
         } catch (Exception ex){
             logger.info("The exception was thrown in wildcardQuery method." + ex);
         }
+
+        return result;
+    }
+
+    public static List<Product> MustQueryName(String searhTerm) throws IOException {
+
+        List<Product> result = new ArrayList<>();
+        Client client = getClient();
+
+        SearchResponse response = client.prepareSearch("onlineshopping")
+                .setQuery(QueryBuilders.multiMatchQuery(searhTerm,"name" , "briefDescription","review").type(MatchQuery.Type.PHRASE_PREFIX))
+                .setExplain(true)
+                .execute()
+                .actionGet();
+
+        SearchHits hits = response.getHits();
+        SearchHit[] searchHits = hits.getHits();
+
+        for (SearchHit hit : searchHits) {
+            System.out.print(hit.getSourceAsString());
+
+            Product doc = gson.fromJson(hit.getSourceAsString(), Product.class);
+            doc.setId(Integer.valueOf(hit.getId()));
+            result.add(doc);
+        }
+
+        for(Product p : result)
+            System.out.print(p);
 
         return result;
     }
